46b9f9a8676d6127cbb7d27e813d800dfd101d9f
John Higgins <johnmatthiggins@gmail.com>
2023-07-22 09:06:56 -0700

    aoesnuth

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import numpy as np
 import sklearn as sk
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
@@ -8,8 +9,18 @@ TRAIN_DATA = 'data/train.csv'
 
 def main():
     train_data = pd.read_csv(TRAIN_DATA)
+
     train_X = pd.get_dummies(train_data.drop('Survived', axis=1).drop('Name', axis=1), dummy_na=True)
+
+    columns = train_X.columns
+
+    def print_nan(row, columns):
+        for column in columns:
+            if pd.isna(row[column]):
+                print(f'{column} column has nan')
+
     print(train_X)
+
     train_Y = train_data[['Survived']]
 
     model = RandomForestClassifier(n_estimators=20)

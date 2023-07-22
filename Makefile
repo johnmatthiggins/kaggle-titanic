c6dc97acb2cf2b42636c0e82ac72656edc67498b
John Higgins <johnmatthiggins@gmail.com>
2023-07-22 08:42:19 -0700

    started writing model.

---
+++ Makefile
@@ -0,0 +1,5 @@
+data:
+	kaggle competitions download -c titanic
+	mkdir data
+	unzip titanic.zip -d data
+
---
+++ model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sklearn as sk
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+
+TEST_DATA = 'data/test.csv'
+TRAIN_DATA = 'data/train.csv'
+
+def main():
+    train_data = pd.read_csv(TRAIN_DATA)
+    train_X = pd.get_dummies(train_data.drop('Survived', axis=1).drop('Name', axis=1))
+    print(train_X)
+    train_Y = train_data[['Survived']]
+
+    model = RandomForestClassifier(n_estimators=20)
+    model.fit(train_X, train_Y)
+
+    y_predictions = model.predict(train_X)
+    print(sk.metrics.accuracy_score(train_Y, y_predictions))
+
+
+if __name__ == '__main__':
+    main()
